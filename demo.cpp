c8085bf21826382e4a821ea536904df34e137da2
hushenglan <hushenglan@gmail.com>
2016-12-25 11:19:12 +0800

    添加boost示例

--- demo.cpp
+++ demo.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <iostream>
+#include <boost/lexical_cast.hpp>
 
 int main(int argc, char *args[])
 {
-    printf("fdsfsdf\n");
+    using boost::lexical_cast;
+    int a = lexical_cast<int>("123456");
+    double b = lexical_cast<double>("123.456");
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    return 0;
 }
