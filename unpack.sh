237bb43a5af27ee05138b201e715eb8d19933d79
hushenglan <hushenglan@gmail.com>
2016-12-28 22:50:19 +0800

    modify unpack

--- unpack.sh
+++ unpack.sh
@@ -2,6 +2,7 @@
 CURRENT_DIR=$PWD
 
 cd $CURRENT_DIR/deps/gtest
+rm -fr ./googletest-release-1.8.0
 tar -zxvf ./googletest-release-1.8.0.tar.gz
 cd googletest-release-1.8.0/googletest/make
 make gtest.a gtest_main.a
