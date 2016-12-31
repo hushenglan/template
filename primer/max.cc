435482b9780c6e182a2c94a9a93077ef4dedecec
hushenglan <hushenglan@gmail.com>
2016-12-31 10:57:49 +0800

    add multiple template param

--- primer/max.cc
+++ primer/max.cc
@@ -24,10 +24,16 @@ TEST_F(TemplateTest, TEST_MAX_4) {
     EXPECT_EQ(my_max_5(4, 3.2), 4);
 }
 
+/*
+ * my_max_5返回的是第一个参数的类型，所以会把6.2强制转换成整形，是6
+ */
 TEST_F(TemplateTest, TEST_MAX_5) {
     EXPECT_EQ(my_max_5(4, 6.2), 6);
 }
 
+/*
+ * my_max_5返回的是第一个参数的类型，所以会把4.2强制转换成整形，是4
+ */
 TEST_F(TemplateTest, TEST_MAX_6) {
     EXPECT_EQ(my_max_5(4, 4.2), 4);
 }
