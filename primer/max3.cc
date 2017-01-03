d1dd4d6dff570ea5be054d346c2e56e9e6a93091
hushenglan <hushenglan@gmail.com>
2017-01-03 23:23:36 +0800

    add max 3

---
+++ primer/max3.cc
@@ -0,0 +1,29 @@
+#include "template_test.h"
+#include "max3.hpp"
+#include <iostream>
+
+TEST_F(TemplateTest, TEST_MAX3_1) {
+    int a = 7;
+    int b = 42;
+    EXPECT_EQ(max_1(a, b), b);
+}
+
+TEST_F(TemplateTest, TEST_MAX3_2) {
+    std::string a = "hey";
+    std::string b = "you";
+    EXPECT_EQ(max_1(a, b), b);
+}
+
+TEST_F(TemplateTest, TEST_MAX3_3) {
+    int a = 7;
+    int b = 42;
+    int* p1 = &a;
+    int* p2 = &b;
+    EXPECT_EQ(max_1(p1, p2), p2);
+}
+
+TEST_F(TemplateTest, TEST_MAX3_4) {
+    char const* s1 = "David";
+    char const* s2 = "Nico";
+    EXPECT_EQ(max_1(s1, s2), s2);
+}
---
+++ primer/max3.hpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+
+template <typename T>
+inline T const& max_1(T const& a, T const& b) {
+    std::cout << "call my_max_1 T" << std::endl;
+    return a > b ? a : b;
+}
+
+template <typename T>
+inline T *const& max_1(T *const& a, T *const& b) {
+    std::cout << "call my_max_1 T pointer" << std::endl;
+    return *a > *b ? a : b;
+}
+
+inline char const* const& max_1(char const* const& a, char const* const& b) {
+    std::cout << "call my_max_1 char*" << std::endl;
+    return std::strcmp(a, b) > 0 ? a : b;
+}
