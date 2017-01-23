9dd67729f46e62299f174e32f530bb9f0b5b665a
hushenglan <hushenglan@gmail.com>
2017-01-23 23:59:14 +0800

    add template class param

---
+++ primer/stack7decl.cc
@@ -0,0 +1,39 @@
+/*
+ * stack7decl.cc
+ *
+ *  Created on: 2017年1月23日
+ *      Author: blue
+ */
+
+#include "template_test.h"
+#include "stack7decl.hpp"
+#include <vector>
+#include <deque>
+#include <stdexcept>
+
+TEST_F(TemplateTest, TEST_STACK7DECL_1) {
+	Stack7<int, std::vector> intStack_1;
+	Stack7<int> intStack_2;
+	Stack7<float> floatStack;
+
+	intStack_1.push(1);
+	intStack_1.push(2);
+	intStack_1.push(3);
+	intStack_1.push(4);
+	intStack_1.push(5);
+	intStack_1.push(6);
+	intStack_1.push(7);
+	intStack_1.push(8);
+
+	std::cout << "start copy to int stack" << std::endl;
+	intStack_2 = intStack_1;
+
+	EXPECT_EQ(intStack_2.top(), 8);
+
+	std::cout << "start copy to float stack" << std::endl;
+	floatStack = intStack_1;
+
+	EXPECT_EQ(floatStack.top(), 8.0);
+}
+
+
---
+++ primer/stack7decl.hpp
@@ -0,0 +1,70 @@
+/*
+ * stack7decl.hpp
+ *
+ *  Created on: 2017年1月23日
+ *      Author: blue
+ */
+
+#ifndef PRIMER_STACK7DECL_HPP_
+#define PRIMER_STACK7DECL_HPP_
+
+#include <deque>
+
+template<typename T, template<typename ELEM, typename ALLOC = std::allocator<ELEM> > class CONT = std::deque >
+class Stack7 {
+private:
+	CONT<T> elems;
+
+public:
+	void push(T const&);
+	void pop();
+	T top() const;
+	bool empty() const {
+		return elems.empty();
+	}
+
+	template<typename T2, template<typename ELEM2, typename ALLOC2 = std::allocator<ELEM2> > class CONT2>
+	Stack7<T, CONT>& operator=(Stack7<T2, CONT2> const&);
+};
+
+template<typename T, template<typename ELEM, typename ALLOC> class CONT>
+template<typename T2, template<typename ELEM2, typename ALLOC2 = std::allocator<ELEM2>> class CONT2>
+Stack7<T, CONT>& Stack7<T, CONT>::operator =(Stack7<T2, CONT2> const& other) {
+	if ((void*)this == (void*)&other) {
+		return *this;
+	}
+
+	Stack7<T2, CONT2> tmp(other);
+	while (!tmp.empty()) {
+		elems.push_front(tmp.top());
+		tmp.pop();
+	}
+
+	return *this;
+}
+
+template<typename T, template<typename, typename> class CONT>
+void Stack7<T, CONT>::push(T const& elem) {
+	elems.push_back(elem);
+}
+
+template<typename T, template<typename, typename> class CONT>
+void Stack7<T, CONT>::pop() {
+	if (elems.empty()) {
+		throw std::out_of_range("empty stack");
+	}
+
+	elems.pop_back();
+}
+
+template<typename T, template<typename, typename> class CONT>
+T Stack7<T, CONT>::top() const {
+	if (elems.empty()) {
+		throw std::out_of_range("empty stack");
+	}
+
+	return elems.back();
+}
+
+
+#endif /* PRIMER_STACK7DECL_HPP_ */
